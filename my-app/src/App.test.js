811751e611eb735969274e3ca7e446500a26b94f
Brooke Pedersen <bpeder2@uw.edu>
2023-07-20 21:25:24 -0700

    done with lab

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -2,14 +2,20 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import userEvent from "@testing-library/user-event";
 
-test('renders learn react link', () => {
+test('Class: testing checkbox', () => {
   render(<App />);
-
   // screen.debug();
   userEvent.click(screen.getByTestId("checkbox"));
-
   expect(screen.getByTestId("checkbox")).toBeChecked()
-
 //   const linkElement = screen.getByText(/learn react/i);
 //   expect(linkElement).toBeInTheDocument();
 });
+test('testing for the words hello world', () => {
+  render(<App />);
+  expect(screen.getByText("Hello World")).toBeInTheDocument();
+  const button = screen.getByRole('button');
+  userEvent.click(button);
+});
+
+
+// npm install --save-dev jest
