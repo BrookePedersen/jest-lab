edb856777a5d8fdf3c542a014082f368727e204f
Brooke Pedersen <bpeder2@uw.edu>
2023-07-20 20:43:17 -0700

    lab stuff

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from "@testing-library/user-event";
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // screen.debug();
+  userEvent.click(screen.getByTestId("checkbox"));
+
+  expect(screen.getByTestId("checkbox")).toBeChecked()
+
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
 });
